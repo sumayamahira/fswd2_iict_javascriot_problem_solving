79f0d2d1b7d7a5d92da4dd8f4de3246319aaeffe
Mahi <coder.sumayamahira@gmail.com>
2026-04-20 14:17:08 +0600

    second commit

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ let value = parseInt(prompt("Please enter a valid number. e.g. 10 : "));
 let calculation = temperature(value);
 console.log(`Now the temperature is ${calculation}(°F)`);
 
+// task-2-You are given an array of numbers. Count how many times the a number is repeated in the array.
+// sample-input: numbers = [5,6,11,12,98, 5], find: 5, output: 2
+// sample-input: numbers = [5,6,11,12,98, 5], find: 25, output: 0
+
+const numbers = [5, 6, 11, 12, 98, 5, 6,6, 12, 10, 11, 23, 24, 45];
+let userInput = parseInt(prompt("Type a valid integer number: "))
+let similar = []
+
+for (let i of numbers){
+    if (i === userInput){
+        similar.push(userInput)
+    }
+
+}
+
+console.log(`The number is ${userInput} and it is ${similar.length} times in the array. `);
+
+
 
 
