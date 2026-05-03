2fbe00049ab1fba10b16aa42a2f13525dbe37d83
Mahi <coder.sumayamahira@gmail.com>
2026-05-03 14:44:45 +0600

    add the template literal on t-9

--- script.js
+++ script.js
@@ -128,28 +128,42 @@
 // Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and
 // returns the average price of phone.
 
-const phones = [
-    { model: "PhoneA", brand: "Iphone", price: 95000 },
-    { model: "PhoneB", brand: "Samsung", price: 40000 },
-    { model: "PhoneC", brand: "Oppo", price: 26000 },
-    { model: "PhoneD", brand: "Nokia", price: 35000 },
-    { model: "PhoneE", brand: "Iphone", price: 105000 },
-    { model: "PhoneF", brand: "HTC", price: 48000 },
-]
+// const phones = [
+//     { model: "PhoneA", brand: "Iphone", price: 95000 },
+//     { model: "PhoneB", brand: "Samsung", price: 40000 },
+//     { model: "PhoneC", brand: "Oppo", price: 26000 },
+//     { model: "PhoneD", brand: "Nokia", price: 35000 },
+//     { model: "PhoneE", brand: "Iphone", price: 105000 },
+//     { model: "PhoneF", brand: "HTC", price: 48000 },
+// ]
 
-function findAveragePhonePrice(phones) {
-    let total = 0;
+// function findAveragePhonePrice(phones) {
+//     let total = 0;
 
-    for (let item of phones) {
-        total += item.price;
-    }
+//     for (let item of phones) {
+//         total += item.price;
+//     }
 
-    let average = total / phones.length;
+//     let average = total / phones.length;
 
-    return average;
-}
+//     return average;
+// }
 
-let averagePrice = findAveragePhonePrice(phones);
-console.log(`The average price of the phones is: ${Math.round(averagePrice)}`);
+// let averagePrice = findAveragePhonePrice(phones);
+// console.log(`The average price of the phones is: ${Math.round(averagePrice)}`);
+
+
+// task-10-For each employee their current salary is calculated by multiplying yearly increment with experience then
+// adding the result to the starting salary. Now calculate is the total salary has to be provided by the company
+// in a month.
+
+
+const employees = [
+
+{ name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+{ name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+{ name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+{ name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
 
 
